c67ccb239a011d214203ae6731e8d1102677d234
fillmello <fillmello@outlook.com>
2025-05-21 19:15:39 -0300

    Add News Page Static

---
+++ public/app.js
@@ -0,0 +1,6 @@
+btnEdit = document.querySelector("#editButton");
+cartaoCrud = document.querySelector("#cartaoCRUD");
+
+btnEdit.addEventListener("click", function () {
+    cartaoCrud.classList.toggle("d-none");
+});
